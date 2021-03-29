a20eb613ed46a590ce1ccb1b2818c8f2ccb77b78
Ari Porad <ari@ariporad.com>
2021-03-29 00:41:59 -0400

    homework 2

---
+++ Robo/hw2/exercise_18_4_6.m
@@ -0,0 +1,35 @@
+function exercise_18_4_6(distance, speed)
+% driveforward is a simple function that controls the NEATO to drive straight forward 
+% at a designated speed for a set distance
+
+%This line says we are going to publish to the topic `/raw_vel' 
+%which are the left and right wheel velocities
+pubvel = rospublisher('/raw_vel') 
+subbump = rossubscriber('/bump')
+
+%Here we are creating a ROS message
+message = rosmessage(pubvel);  
+
+%Set the right and left wheel velocities 
+message.Data = [speed, speed]; 
+
+% Send the velocity commands to the NEATO
+send(pubvel, message);
+
+% rostic allows us to do timing based on the simulator clock
+rostic;
+while 1
+    % calculate the delta t from when we started moving using rostoc
+    elapsed = rostoc;
+    if elapsed > distance/speed % Here we are saying the if the elapsed time is greater than 
+        %distance/speed, we have reached our desired distance and we should stop
+        
+        message.Data = [0,0]; % set wheel velocities to zero if we have reached the desire distance
+        send(pubvel, message); % send new wheel velocities
+        break %leave this loop once we have reached the stopping time
+    end
+    bumpMessage = receive(subbump);
+    if any(bumpMessage.Data)
+        driveforward(speed * -5, -speed);
+    end
+end
