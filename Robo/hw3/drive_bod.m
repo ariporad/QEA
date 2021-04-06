d3154c351ff31c4c5862aa23b717e9f5e422a86d
Ari Porad <ari@ariporad.com>
2021-04-05 20:00:01 -0400

    hw3: robot drives the bridge of doom

--- Robo/hw3/drive_bod.m
+++ Robo/hw3/drive_bod.m
@@ -1,36 +1,59 @@
 function drive_bod()
+
 % Insert any setup code you want to run here
+%% Robot Config
+alpha = 1/5;
+d = 0.235; % m
+U_RANGE = [0 3.2];
+T_RANGE = U_RANGE ./ alpha;
+% N = 50;
+% 
+% us = linspace(U_RANGE(1), U_RANGE(2), N)';
+% ts = us ./ alpha;
 equations
+msgData = [v_l, v_r];
+generateMessageData = matlabFunction(msgData);
+
+disp("Connecting to Neato...")
 
 pub = rospublisher('raw_vel');
+
+disp("Stopping Neato...")
 
 % stop the robot if it's going right now
 stopMsg = rosmessage(pub);
 stopMsg.Data = [0 0];
 send(pub, stopMsg);
 
-bridgeStart = double(subs(R,t,0));
-startingThat = double(subs(That,t,0));
+disp("Resetting Neato...")
+
+bridgeStart = double(subs(r,t,0));
+startingThat = double(subs(T_hat,t,0));
 placeNeato(bridgeStart(1),  bridgeStart(2), startingThat(1), startingThat(2));
 
 % wait a bit for robot to fall onto the bridge
 pause(2);
 
-% time to drive!!
+disp("Starting to Drive...")
 
+% time to drive!!
 rostic;
 
 while 1
     cur_t = rostoc;
     
-    if cur_t > T_RANGE(2)
+    if cur_t >= T_RANGE(2)
+        stopMsg = rosmessage(pub);
+        stopMsg.Data = [0 0];
+        send(pub, stopMsg);
         break
     end
     
     speedMsg = rosmessage(pub);
-    % TODO: Convert this to use matlabFunction ahead of time
-    % TODO: Could we pack these into a vector ahead of time?
-    speedMsg.Data = [double(subs(v_l, t, cur_t)), double(subs(v_r, t, cur_t))]
+    speedMsg.Data = generateMessageData(cur_t);
+    send(pub, speedMsg);
 end
+
+disp("Done!")
 
 end
