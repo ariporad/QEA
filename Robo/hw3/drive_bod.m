549a5b170f148ea45b6d9cea49c16f912eb11df3
Ari Porad <ari@ariporad.com>
2021-04-06 23:29:26 -0400

    hw3: finish the Bridge of Doom (dun dun dun)

--- Robo/hw3/drive_bod.m
+++ Robo/hw3/drive_bod.m
@@ -1,59 +1,82 @@
 function drive_bod()
+    %% Configuration
+    % alpha is a linear scalar with time, and controls the speed of the robot
+    % It's been approximately calibrated to keep the robot's maximum speed
+    % below the limit of 2 m/s
+    alpha = 1/5;
 
-% Insert any setup code you want to run here
-%% Robot Config
-alpha = 1/5;
-d = 0.235; % m
-U_RANGE = [0 3.2];
-T_RANGE = U_RANGE ./ alpha;
-% N = 50;
-% 
-% us = linspace(U_RANGE(1), U_RANGE(2), N)';
-% ts = us ./ alpha;
-equations
-msgData = [v_l, v_r];
-generateMessageData = matlabFunction(msgData);
+    % d is the wheelbase of the robot
+    d = 0.235; % m
 
-disp("Connecting to Neato...")
+    % The range of u is specified by the definition of the Bridge of Doom (dun
+    % dun dun)
+    U_RANGE = [0 3.2];
 
-pub = rospublisher('raw_vel');
+    % We also convert the range of u into a range of t (u and t are linearly
+    % correlated) for convinence
+    T_RANGE = U_RANGE ./ alpha;
 
-disp("Stopping Neato...")
+    % This loads the equations from the cannonical equations.m file, which we
+    % use to ensure that this script and the main live script have exactly the
+    % same equations, in line with the programming principle of DRY (Don't
+    % Repeat Yourself).
+    equations;
 
-% stop the robot if it's going right now
-stopMsg = rosmessage(pub);
-stopMsg.Data = [0 0];
-send(pub, stopMsg);
+    % This is the format of the messages we send to the Neato. By packing it
+    % into a vector here and converting that from a symbolic value into a
+    % matlab function, it becomes much much faster to send commands to the
+    % Neato, which increases accuracy.
+    msgData = [v_l, v_r];
 
-disp("Resetting Neato...")
+    % generateMessageData(t) now returns a vector with the right v_l and v_r,
+    % without using the Symbolic Toolbox
+    generateMessageData = matlabFunction(msgData);
 
-bridgeStart = double(subs(r,t,0));
-startingThat = double(subs(T_hat,t,0));
-placeNeato(bridgeStart(1),  bridgeStart(2), startingThat(1), startingThat(2));
+    %% Connect to the Neato 
+    disp("Connecting to Neato...")
+    pub = rospublisher('raw_vel');
 
-% wait a bit for robot to fall onto the bridge
-pause(2);
+    %% Setup the Neato
+    disp("Stopping Neato...")
 
-disp("Starting to Drive...")
+    % stop the robot if it's going right now
+    stopMsg = rosmessage(pub);
+    stopMsg.Data = [0 0];
+    send(pub, stopMsg);
 
-% time to drive!!
-rostic;
+    disp("Resetting Neato...")
 
-while 1
-    cur_t = rostoc;
-    
-    if cur_t >= T_RANGE(2)
-        stopMsg = rosmessage(pub);
-        stopMsg.Data = [0 0];
-        send(pub, stopMsg);
-        break
+    bridgeStart = double(subs(r,t,0));
+    startingThat = double(subs(T_hat,t,0));
+    placeNeato(bridgeStart(1),  bridgeStart(2), startingThat(1), startingThat(2));
+
+    % HACK: Wait a bit for robot to fall onto the bridge
+    pause(2);
+
+    %% Drive
+    disp("Starting to Drive...")
+
+    % Keep track of when we started so we can calculate t 
+    rostic;
+
+    while 1 % We'll break out of the loop manually
+        % Get the current time
+        cur_t = rostoc;
+
+        % If we've hit the end, stop the robot and exit the loop
+        if cur_t >= T_RANGE(2)
+            stopMsg = rosmessage(pub);
+            stopMsg.Data = [0 0];
+            send(pub, stopMsg);
+            break
+        end
+
+        % Otherwise, calculate the speed the Neato should be at and tell it to
+        % go at that speed
+        speedMsg = rosmessage(pub);
+        speedMsg.Data = generateMessageData(cur_t);
+        send(pub, speedMsg);
     end
-    
-    speedMsg = rosmessage(pub);
-    speedMsg.Data = generateMessageData(cur_t);
-    send(pub, speedMsg);
-end
 
-disp("Done!")
-
+    disp("Done!")
 end
