947affbc2d7db089bf03583031c06d60230de1e0
Ari Porad <ari@ariporad.com>
2021-05-02 16:08:50 -0400

    gauntlet: work in progress on scans

---
+++ Robo/gauntlet/collectScans.m
@@ -0,0 +1,32 @@
+% lidar.mat has a lidar matrix which is n x 2 x m, where each (:, :, m) is
+% one scan in the form of [r, theta], where r and theta are column vectors.
+% It also has a 3 x m matrix of column vectors of the format [x, y, phi]
+
+function lidar_results = collectScans(lidar_positions, filename)
+    sub = rossubscriber('/scan');
+    
+    % setup
+    num_scans = size(lidar_positions, 1);
+    lidar_results = NaN(360, 2, num_scans);
+    
+    for scan_idx=1:num_scans
+        % place Neato at the appropriate point
+        angle = lidar_positions(scan_idx, 3);
+        placeNeato(lidar_positions(scan_idx, 1), lidar_positions(scan_idx, 2), cos(angle), sin(angle))
+
+        % wait a while for the Neato to fall into place
+        pause(2);
+
+        % then collect data for the second location
+        scan_message = receive(sub);
+        
+        % store it appropriately
+        lidar_results(:, 1, scan_idx) = scan_message.Ranges(1:end-1);
+        lidar_results(:, 2, scan_idx) = deg2rad([0:359]');
+    end
+    
+    % save the data if we were given a filename
+    if nargsin >= 2
+        save(filename, "lidar_results", "lidar_positions");
+    end
+end
---
+++ Robo/gauntlet/doScan.m
@@ -0,0 +1,10 @@
+positions = [
+    % x, y, phi
+      0,    0, 0;
+      0,   -3, 0;
+    2.5,    0, 0;
+    1.5, -1.5, 0;
+    2.5, -2.5, 0;
+];
+
+collectScans(positions, "lidar.mat");
